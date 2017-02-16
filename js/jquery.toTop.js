35f497e0a6dd0823920e27f40be87b0ef5e17cd0
AllocotorXy <allocatorxy@foxmail.com>
2017-02-16 23:16:35 +0800

    s

--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -20,11 +20,11 @@ function btnToTop() {
                    oBtn.stop().fadeOut('fast');
     });
     // if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-        oBtn.on('touchstart', function(event) {
+        oBtn.on('touchstart', function() {
             $('html, body').stop().animate({ scrollTop: 0 }, 'fast');
             oBtn.css('transform', 'scale(1.2, 1.2)');
         });
-        oBtn.on('touchend', function(event) {
+        oBtn.on('touchend', function() {
             oBtn.css('transform', 'scale(1.0, 1.0)');
         });
     // } else {
