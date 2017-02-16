4634552ea5553d60442e033d441f57f6e25a67e1
AllocotorXy <allocatorxy@foxmail.com>
2017-02-16 22:47:47 +0800

    s

--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -16,22 +16,22 @@ function btnToTop() {
     const oBtn = $('#btnToTop');
     $(window).scroll(function() {
         const s = $(window).scrollTop();
-        s >= 700 ? oBtn.show() :
+        s >= 500 ? oBtn.show() :
                    oBtn.stop().fadeOut('fast');
     });
-    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-        oBtn.ontouchstart = function() {
-            $('html, body').animate({ scrollTop: 0 }, 'fast');
+    // if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+        oBtn.on('touchstart', function(event) {
+            $('html, body').stop().animate({ scrollTop: 0 }, 'fast');
             oBtn.css('transform', 'scale(1.2, 1.2)');
-        };
-        oBtn.ontouchend = function() {
-            oBtn.css('transform', 'scale(1.0, 1.0)');
-        };
-    } else {
-        oBtn.click(function() {
-            $('html, body').animate({ scrollTop: 0 }, 'fast');
         });
-    }
+        oBtn.on('touchend', function(event) {
+            oBtn.css('transform', 'scale(1.0, 1.0)');
+        });
+    // } else {
+    //     oBtn.click(function() {
+    //         $('html, body').animate({ scrollTop: 0 }, 'fast');
+    //     });
+    // }
 }
 $(document).ready(function() {
     btnToTop();
