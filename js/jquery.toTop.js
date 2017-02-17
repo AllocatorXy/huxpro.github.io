2a22c58ffa5dd620b6fd74ef7ef65dd5e7e948b2
AllocotorXy <allocatorxy@foxmail.com>
2017-02-17 11:03:41 +0800

    s

--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -1,14 +1,12 @@
-(function($) {
-    'use strict';
-    function btnToTop() {
-        const oBtn = $('#btnToTop');
-        $(window).scroll(function() {
-            const s = $(window).scrollTop();
-            s >= 500 ? oBtn.show() :
-                       oBtn.stop().fadeOut('fast');
-        });
-        oBtn.click(function() {
-            $('html, body').animate({ scrollTop: 0 }, 'fast');
-        });
-    }
-})(jQuery);
+'use strict';
+(function btnToTop() {
+    const oBtn = $('#btnToTop');
+    $(window).scroll(function() {
+        const s = $(window).scrollTop();
+        s >= 500 ? oBtn.show() :
+                   oBtn.stop().fadeOut('fast');
+    });
+    oBtn.click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 'fast');
+    });
+})();
