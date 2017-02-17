07959242c9f515f0b74cd33523500a352f3e8cca
AllocotorXy <allocatorxy@foxmail.com>
2017-02-17 10:34:04 +0800

    asd

--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -19,22 +19,9 @@ function btnToTop() {
         s >= 500 ? oBtn.show() :
                    oBtn.stop().fadeOut('fast');
     });
-    // if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-        oBtn.on('touchstart', function() {
-            $('html, body').stop().animate({ scrollTop: 0 }, 'fast');
-            oBtn.css('transform', 'scale(1.2, 1.2)');
-        });
-        oBtn.on('touchmove', function() {
-            return false;
-        });
-        oBtn.on('touchend', function() {
-            oBtn.css('transform', 'scale(1.0, 1.0)');
-        });
-    // } else {
-    //     oBtn.click(function() {
-    //         $('html, body').animate({ scrollTop: 0 }, 'fast');
-    //     });
-    // }
+    oBtn.click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 'fast');
+    });
 }
 $(document).ready(function() {
     btnToTop();
