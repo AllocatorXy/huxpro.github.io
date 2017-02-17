4a6a7ad5c193c2336757971657d2ba124d2d737c
AllocotorXy <allocatorxy@foxmail.com>
2017-02-17 10:53:30 +0800

    s

--- js/jquery.toTop.js
+++ js/jquery.toTop.js
@@ -1,29 +1,14 @@
-// 'use strict';
-// function btnToTop() {
-//     const oBtn = $('#btnToTop');
-//     $(window).scroll(function() {
-//         const s = $(window).scrollTop();
-//         s >= 700 ? oBtn.show() :
-//                    oBtn.stop().fadeOut('fast');
-//     });
-//     oBtn.click(function() {
-//         $('html, body').animate({ scrollTop: 0 }, 'fast');
-//     });
-// }
-
-'use strict';
-function btnToTop() {
-    const oBtn = $('#btnToTop');
-    $(window).scroll(function() {
-        const s = $(window).scrollTop();
-        s >= 500 ? oBtn.show() :
-                   oBtn.stop().fadeOut('fast');
-    });
-    oBtn.click(function() {
-        $('html, body').animate({ scrollTop: 0 }, 'fast');
-    });
-}
-$(document).ready(function() {
-    btnToTop();
-});
-
+(function($) {
+    'use strict';
+    function btnToTop() {
+        const oBtn = $('#btnToTop');
+        $(window).scroll(function() {
+            const s = $(window).scrollTop();
+            s >= 500 ? oBtn.show() :
+                       oBtn.stop().fadeOut('fast');
+        });
+        oBtn.click(function() {
+            $('html, body').animate({ scrollTop: 0 }, 'fast');
+        });
+    }
+})(jQuery);
